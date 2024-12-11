9435e09871abb5a8df6da993b6bc3050bf2b9bd7
EricaAkos <ericadeku4@gmail.com>
2024-12-11 10:18:52 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+let heading = document.getElementById("heading");
+heading.style.backgroundColor = "blue";
+heading.style.color = "white";
+heading.style.textAlign = "center";
+heading.style.fontStyle = "italic";
+
+let quest = document.getElementById("quest");
+quest.innerHTML = ("<p>I know now what the DOM is 🙌</p>");
+quest.style.backgroundColor = "grey";
+quest.style.color = "black";
+
+let quest1 = document.getElementById("quest1");
+quest1.innerHTML = ("<p>I know how to access/select elements in the DOM 👊");
+quest1.style.backgroundColor = "yellow";
+quest1.style.color = "black"
+
+let quest2 = document.getElementById("quest2");
+quest2.innerHTML = ("<p>I know how to use the style methods to change the style of an element in the DOM ✅");
+quest2.style.backgroundColor = "green";
+quest2.style.color = "black"
+
+let quest3 = document.getElementById("quest3");
+quest3.innerHTML = ("<p>I know how the styling properties differ in JS from CSS 👍");
+quest3.style.backgroundColor = "purple";
+quest3.style.color = "black"
+
+let quest4 = document.getElementById("quest4");
+quest4.innerHTML = ("<p>Yes I did it!! I manipulated the DOM and styled my elements 🥰🥰🥰🥰");
+quest4.style.backgroundColor = "black";
+quest4.style.color = "white"
