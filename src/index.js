0b509eae4b2a3c602345ab93d519c29b89f6767b
EricaAkos <ericadeku4@gmail.com>
2024-12-11 22:10:48 +0000

    changed the codes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
-let heading = document.getElementById("heading");
-heading.style.backgroundColor = "blue";
-heading.style.color = "white";
-heading.style.textAlign = "center";
-heading.style.fontStyle = "italic";
-
-let quest = document.getElementById("quest");
-quest.innerHTML = ("<p>I know now what the DOM is 🙌</p>");
-quest.style.backgroundColor = "grey";
-quest.style.color = "black";
-
-let quest1 = document.getElementById("quest1");
-quest1.innerHTML = ("<p>I know how to access/select elements in the DOM 👊");
-quest1.style.backgroundColor = "yellow";
-quest1.style.color = "black"
-
-let quest2 = document.getElementById("quest2");
-quest2.innerHTML = ("<p>I know how to use the style methods to change the style of an element in the DOM ✅");
-quest2.style.backgroundColor = "green";
-quest2.style.color = "black"
-
-let quest3 = document.getElementById("quest3");
-quest3.innerHTML = ("<p>I know how the styling properties differ in JS from CSS 👍");
-quest3.style.backgroundColor = "purple";
-quest3.style.color = "black"
-
-let quest4 = document.getElementById("quest4");
-quest4.innerHTML = ("<p>Yes I did it!! I manipulated the DOM and styled my elements 🥰🥰🥰🥰");
-quest4.style.backgroundColor = "black";
-quest4.style.color = "white"
+document.getElementById("quest1").addEventListener("click", function() {
+    const quest1 = document.getElementById('quest1');
+      quest1.innerHTML = 'I know now what the DOM is 🙌';
+      quest1.style.backgroundColor = "grey";
+      quest1.style.color = "black";
+  });
+  
+  document.getElementById("quest2").addEventListener("click", function() {
+      const quest2 = document.getElementById("quest2");
+      quest2.innerHTML = "I know how to access/select elements in the DOM 👊";
+      quest2.style.backgroundColor = "yellow";
+      quest2.style.color = "black"
+  });
+  
+  document.getElementById("quest3").addEventListener("click", function() {
+      const quest3 = document.getElementById("quest3");
+      quest3.innerHTML = "I know how to use the style methods to change the style of an element in the DOM ✅";
+      quest3.style.backgroundColor = "green";
+      quest3.style.color = "black"
+  });
+  
+  document.getElementById("quest4").addEventListener("click", function() {
+      const quest4 = document.getElementById('quest4');
+      quest4.innerHTML = "I know how the styling properties differ in JS from CSS 👍";
+      quest4.style.backgroundColor = "purple";
+      quest4.style.color = "black"
+  });
+  
+  document.getElementById("quest5").addEventListener("click", function() {
+      const quest5 = document.getElementById('quest5');
+      quest5.innerHTML = "Yes I did it!! I manipulated the DOM and styled my elements 🤩🤩🤩🤩";
+      quest5.style.backgroundColor = "black";
+      quest5.style.color = "white"
+  });
